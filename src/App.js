9b757059b7ea0f242b88e2ed576c70f1fc3a49e5
LeeTaylor-git <71027054+LeeTaylor-git@users.noreply.github.com>
2022-11-10 22:32:25 -0700

    update
    initial init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+ * Project Name: React Navbar
+ * Author: Taylor Lee
+ * Last modified: November 11th, 2022
+ * Desc: You will create a fully responsive navbar on React using CSS grid and flexbox. Additionally, display
+ *       breadcrumbs with the menu dropdown on mobile screen size.
+ */
 
-function App() {
+
+//import statements
+import React from "react";
+import Header from "./header/Header";
+import "./styles.css";
+
+
+//code to start the app
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <h3 style={{ padding: "40px" }}>Welcome to my Navbar App!</h3>
     </div>
   );
 }
-
-export default App;
---
+++ src/header/Header.jsx
@@ -0,0 +1,66 @@
+/*
+ * Project Name: React Navbar
+ * Author: Taylor Lee
+ * Last modified: November 11th, 2022
+ * Desc: You will create a fully responsive navbar on React using CSS grid and flexbox. Additionally, display
+ *       breadcrumbs with the menu dropdown on mobile screen size.
+ */
+
+
+//import statements
+import React, { useState } from "react";
+import { ReactComponent as CloseMenu } from "../assets/x.svg";
+import { ReactComponent as MenuIcon } from "../assets/menu.svg";
+import "./header.css";
+
+
+//code for the header, the meat of the application.
+const Header = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  return (
+    <div className="header">
+      <div className="logo-nav">
+        <ul className={click ? "nav-options active" : "nav-options"}>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">HOME</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">ABOUT</a>
+          </li>
+          <li className="option" onClick={closeMobileMenu}>
+            <a href="#">CONTACT</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="#">SIGN-IN</a>
+          </li>
+          <li className="option mobile-option" onClick={closeMobileMenu}>
+            <a href="" className="sign-up">
+              SIGN-UP
+            </a>
+          </li>
+        </ul>
+      </div>
+      <ul className="signin-up">
+        <li className="sign-in" onClick={closeMobileMenu}>
+          <a href="#">SIGN-IN</a>
+        </li>
+        <li onClick={closeMobileMenu}>
+          <a href="" className="signup-btn">
+            SIGN-UP
+          </a>
+        </li>
+      </ul>
+      <div className="mobile-menu" onClick={handleClick}>
+        {click ? (
+          <CloseMenu className="menu-icon" />
+        ) : (
+          <MenuIcon className="menu-icon" />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Header;
